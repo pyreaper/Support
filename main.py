076a80ffe63761941000717da0ba7d871b0d66ee
_thecoffee_ <angryband001@gmail.com>
2024-02-24 11:58:54 +0300

    пинг

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ intents.members = True
 from dotenv import load_dotenv
 load_dotenv()
 
+version = "2.2.5"
 token = os.getenv("TOKEN")
 
 bot = commands.InteractionBot(intents=intents)
@@ -24,5 +25,16 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
         print(f"[MAIN] cogs.{filename[:-3]} загружен")
+
+@bot.slash_command(name="ping", description="Пинг бота, и другая полезная информация")
+async def ping(inter: disnake.ApplicationCommandInteraction):
+    embed = disnake.Embed(
+        title="🏓  Понг",
+        description=f"> **🟢 Задержка бота: {round(bot.latency * 1000)}мс \n > <:cmt:1210629320356266005> Версия бота: {version}**"
+    )
+
+    embed.set_image(url="https://cdn.discordapp.com/attachments/1083744274912378884/1083747819845865593/line.png?ex=65eb16cf&is=65d8a1cf&hm=f04e5f39e348595fa4824fa1a3833f1feaaa7df21195ede7dd5d76d3529c47fe&")
+
+    await inter.response.send_message(embeds=[embed], ephemeral=True)
 
 bot.run(os.getenv('TOKEN'))
